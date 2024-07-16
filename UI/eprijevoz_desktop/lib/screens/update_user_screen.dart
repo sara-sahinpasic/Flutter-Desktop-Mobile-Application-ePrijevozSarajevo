bd9228149ec7e37aba78bc16de4fe1942764b6d8
Sara Sahinpasic <sara.sahinpasic@hotmail.com>
2024-07-16 18:13:23 +0200

    Add: Update_user_screen

---
+++ UI/eprijevoz_desktop/lib/screens/update_user_screen.dart
@@ -0,0 +1,283 @@
+import 'dart:async';
+
+import 'package:eprijevoz_desktop/models/search_result.dart';
+import 'package:eprijevoz_desktop/models/status.dart';
+import 'package:eprijevoz_desktop/models/user.dart';
+import 'package:eprijevoz_desktop/providers/status_provider.dart';
+import 'package:eprijevoz_desktop/providers/user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:provider/provider.dart';
+
+class UpdateUserDialog extends StatefulWidget {
+  Status? status;
+  User? user;
+  final VoidCallback onUpdate;
+  UpdateUserDialog({super.key, this.status, this.user, required this.onUpdate});
+
+  @override
+  State<UpdateUserDialog> createState() => _UpdateUserDialogState();
+}
+
+class _UpdateUserDialogState extends State<UpdateUserDialog> {
+  late UserProvider userProvider;
+  late StatusProvider statusProvider;
+  SearchResult<User>? userResult;
+  SearchResult<Status>? statusResult;
+  SearchResult<User>? userResultForStatus;
+
+  int _selectedStatusId = 0;
+
+  bool isLoading = true;
+
+//Form
+  final _formKey = GlobalKey<FormBuilderState>();
+  Map<String, dynamic> _initialValue = {};
+
+  @override
+  void initState() {
+    userProvider = context.read<UserProvider>();
+    statusProvider = context.read<StatusProvider>();
+    super.initState();
+
+    _initialValue = {
+      'firstName': widget?.user?.firstName,
+      'lastName': widget?.user?.lastName,
+      'userName': widget?.user?.userId,
+      'dateOfBirth': widget?.user?.dateOfBirth?.toString(),
+      'phoneNumber': widget?.user?.phoneNumber,
+      'address': widget?.user?.address,
+      'userStatusId': widget?.user?.userStatusId?.toString(),
+      'statusId': widget?.status?.statusId?.toString(),
+    };
+
+    initForm();
+  }
+
+  Future initForm() async {
+    userResult = await userProvider.get();
+    statusResult = await statusProvider.get();
+
+    print("zahtjev: ${userResult?.result.length}");
+    print("zahtjev1: ${userResult?.result.map((e) => e.userStatusId)}");
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  final TextEditingController _ftsFirstNameController = TextEditingController();
+  final TextEditingController _ftsLastNameController = TextEditingController();
+  final TextEditingController _ftsPhoneController = TextEditingController();
+  final TextEditingController _ftsAddressController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Update"),
+      content: Container(
+        // color: Colors.red,
+        width: 500,
+        height: 450,
+        child: FormBuilder(
+          key: _formKey,
+          initialValue: _initialValue,
+          child: Column(children: [
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Ime:",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                const SizedBox(
+                  width: 80,
+                ),
+                Expanded(
+                  child: FormBuilderTextField(
+                    name: 'firstName',
+                    controller: _ftsFirstNameController,
+                    cursorColor: Colors.green.shade800,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Prezime:",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                const SizedBox(
+                  width: 50,
+                ),
+                Expanded(
+                  child: FormBuilderTextField(
+                    name: 'lastName',
+                    controller: _ftsLastNameController,
+                    cursorColor: Colors.green.shade800,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Broj telefona:",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: FormBuilderTextField(
+                    name: 'phoneNumber',
+                    controller: _ftsPhoneController,
+                    cursorColor: Colors.green.shade800,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Adresa:",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                const SizedBox(
+                  width: 60,
+                ),
+                Expanded(
+                  child: FormBuilderTextField(
+                    name: 'address',
+                    controller: _ftsAddressController,
+                    cursorColor: Colors.green.shade800,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                const Text(
+                  "Status:",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                const SizedBox(
+                  width: 60,
+                ),
+                Expanded(
+                  child: FormBuilderDropdown(
+                    name: "statusId",
+                    items: statusResult?.result
+                            .map((item) => DropdownMenuItem(
+                                value: item.statusId.toString(),
+                                child: Text(item.name ?? "")))
+                            .toList() ??
+                        [],
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedStatusId = int.parse(value as String);
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: ElevatedButton(
+                  onPressed: () async {
+                    _formKey.currentState?.saveAndValidate();
+                    var request = Map.from(_formKey.currentState!.value);
+                    // Update userStatusId with the selected statusId
+                    request['userStatusId'] = _selectedStatusId;
+
+                    if (widget.user != null) {
+                      await userProvider.update(widget.user!.userId!, request);
+                      Navigator.pop(context);
+                    }
+
+                    print("Testtt: ${widget.user!.userId!}, ${request}}");
+                    widget.onUpdate();
+
+                    showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                              title: Text("Update"),
+                              content: Text("Korisnik je ažuriran"),
+                              actions: [
+                                TextButton(
+                                  child: Text(
+                                    "OK",
+                                    style: TextStyle(color: Colors.green),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                )
+                              ],
+                            ));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(72, 156, 118, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(2.0),
+                    ),
+                    minimumSize: const Size(100, 65),
+                  ),
+                  child: const Text("Ažuriraj", style: TextStyle(fontSize: 18)),
+                )),
+              ],
+            ),
+          ]),
+        ),
+      ),
+      actions: [
+        TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text(
+              "Cancel",
+              style: TextStyle(color: Colors.red, fontSize: 18),
+            )),
+      ],
+    );
+  }
+}
--- ePrijevozSarajevo/ePrijevozSarajevo.Model/Requests/UserUpdateRequest.cs
+++ ePrijevozSarajevo/ePrijevozSarajevo.Model/Requests/UserUpdateRequest.cs
@@ -6,12 +6,12 @@
         public string LastName { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
         public string? Address { get; set; }
-        public DateTime ModifiedDate { get; set; }
-        public bool Active { get; set; }
         public int? UserStatusId { get; set; }
-        public DateTime? StatusExpirationDate { get; set; }
-        public string Password { get; set; } = null!;
-        public string PasswordConfirmation { get; set; } = null!;
-        public string? UserName { get; set; }
+        public DateTime ModifiedDate { get; set; } = DateTime.UtcNow;
+        public string? Password { get; set; }
+        public string? PasswordConfirmation { get; set; }
+        // public bool Active { get; set; }
+        // public DateTime? StatusExpirationDate { get; set; }
+        //string Password { get; set; } = null!;
     }
 }
