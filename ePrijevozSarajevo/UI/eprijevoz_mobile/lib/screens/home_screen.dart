4cb1a04aae2bb26e26c788347b3d0e281b464858
Sara Sahinpasic <sara.sahinpasic@hotmail.com>
2024-10-15 17:12:54 +0200

    Add: UI for mobile home_screen Fix: mobile route_search_screen

--- ePrijevozSarajevo/UI/eprijevoz_mobile/lib/screens/home_screen.dart
+++ ePrijevozSarajevo/UI/eprijevoz_mobile/lib/screens/home_screen.dart
@@ -1,4 +1,11 @@
+import 'package:eprijevoz_mobile/layouts/master_screen.dart';
+import 'package:eprijevoz_mobile/main.dart';
+import 'package:eprijevoz_mobile/models/search_result.dart';
+import 'package:eprijevoz_mobile/models/user.dart';
+import 'package:eprijevoz_mobile/providers/auth_provider.dart';
+import 'package:eprijevoz_mobile/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,19 +14,128 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late UserProvider userProvider;
+  SearchResult<User>? userResult;
+  User? currentUser;
+
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _buildResultVIew(),
-      ],
-    );
+  void initState() {
+    userProvider = context.read<UserProvider>();
+
+    initForm();
+
+    super.initState();
   }
 
-  Widget _buildResultVIew() {
-    return Expanded(
-      child: Center(
-        child: Text("Home Page Content"),
+  Future initForm() async {
+    userResult = await userProvider.get();
+    currentUser = userResult?.result
+        .firstWhere((user) => user.userName == AuthProvider.username);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(15.0, 37.0, 15.0, 0.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Flexible(
+                    child: Text(
+                      "Dobrodošli, ${currentUser?.firstName} ${currentUser?.lastName}!",
+                      style: const TextStyle(
+                          fontSize: 19, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => LoginPage())),
+                    child: const Text(
+                      "Odjava",
+                      style: TextStyle(
+                        color: Colors.red,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.red,
+                        decorationThickness: 3.0,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 85),
+              const Row(
+                children: [
+                  Flexible(
+                    child: Text(
+                      "Želimo Vam sigurnu i ugodnu vožnju javnim gradskim prijevozom!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 85),
+              Row(
+                children: [
+                  Flexible(
+                    child: Text(
+                      "ZAJEDNO ZAŠTITIMO NAŠU PLANETU ZEMLJU!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 21,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green.shade700,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 85),
+              Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => MasterScreen(
+                                    initialIndex: 1,
+                                  )));
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        ),
+                        child: const Text(
+                          "Krenimo odmah!",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
