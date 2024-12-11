188656054a1928e2e14919de634d658fac4f4ca7
Sara Sahinpasic <sara.sahinpasic@hotmail.com>
2024-12-11 22:52:37 +0100

    Add: ML Recommender system

--- ePrijevozSarajevo/UI/eprijevoz_mobile/lib/providers/route_provider.dart
+++ ePrijevozSarajevo/UI/eprijevoz_mobile/lib/providers/route_provider.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:eprijevoz_mobile/models/route.dart';
 import 'package:eprijevoz_mobile/providers/base_provider.dart';
+import 'package:http/http.dart' as http;
 
 class RouteProvider extends BaseProvider<Route> {
   RouteProvider() : super("Route");
@@ -7,5 +10,23 @@ class RouteProvider extends BaseProvider<Route> {
   @override
   Route fromJson(data) {
     return Route.fromJson(data);
+  }
+
+  Future<List<Route>> getRecommendations(int userId,
+      {int maxRecommendations = 5}) async {
+    final url =
+        ("${BaseProvider.baseUrl}$endpoint/recommendations/$userId?maxRecommendations=$maxRecommendations");
+    var uri = Uri.parse(url);
+    var headers = createHeaders();
+
+    final response = await http.get(uri, headers: headers);
+
+    if (isValidResponse(response)) {
+      // Parse the response body and convert it to a list of Route objects
+      final List<dynamic> data = jsonDecode(response.body);
+      return data.map((json) => Route.fromJson(json)).toList();
+    } else {
+      throw Exception("Failed to get recommendations");
+    }
   }
 }
